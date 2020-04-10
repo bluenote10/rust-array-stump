446a4a83907cd1b91be8f07d58629f00818a6c96
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-04-10 19:04:46 +0200

    added different generators for stats analysis

--- benchmarks/src/bin/stats.rs
+++ benchmarks/src/bin/stats.rs
@@ -1,5 +1,5 @@
-extern crate array_stump_benchmarks;
-extern crate array_stump;
+
+use clap::{App, AppSettings, Arg};
 
 use array_stump_benchmarks::create_cmp;
 use array_stump_benchmarks::helpers;
@@ -12,9 +12,27 @@ use pretty_assertions::assert_eq;
 create_cmp!(cmp_array_tree, get_num_calls_array_tree, NUM_CALLS_ARRAY_TREE);
 
 fn run_fill_statistics() {
+    #[rustfmt::skip]
+    let matches = App::new("Benchmark runner")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::with_name("gen-mode")
+                 .long("gen-mode")
+                 .short("g")
+                 .default_value("avg")
+                 .possible_values(&["avg", "asc", "dsc"])
+                 .help("Generator mode"))
+        .get_matches();
+
+    let gen_mode = matches.value_of("gen-mode").unwrap().to_string();
+
     let n = 1000000;
     let measure_every = 10;
-    let values = helpers::gen_rand_values(n);
+    let values = match gen_mode.as_ref() {
+        "avg" => helpers::gen_rand_values(n),
+        "asc" => (0 .. n).map(|x| x as f64 / n as f64).collect(),
+        "dsc" => (0 .. n).map(|x| x as f64 / n as f64).rev().collect(),
+        _ => panic!("Invalid generator mode")
+    };
 
     let mut set = ArrayStump::new(cmp_array_tree, 256);
 
@@ -27,7 +45,7 @@ fn run_fill_statistics() {
     let mut capacity = Vec::new();
     let mut num_cmp_calls = Vec::new();
 
-
+    println!("Inserting...");
     let start = Instant::now();
     for (i, x) in values.iter().enumerate() {
         set.insert(*x);
@@ -48,6 +66,7 @@ fn run_fill_statistics() {
     let mut values_to_remove = values.to_vec();
     helpers::shuffle(&mut values_to_remove);
 
+    println!("Removing...");
     for (i, x) in values_to_remove.iter().enumerate() {
         set.remove(x);
         let len = i + 1;
@@ -63,6 +82,7 @@ fn run_fill_statistics() {
         }
     }
 
+    println!("Exporting...");
     helpers::export_stats(&iters, &times, &fill_ratio, &fill_min, &fill_max, &num_blocks, &capacity, &num_cmp_calls);
     helpers::call_plots_stats();
 }
